a9198ddc5da541ac3feb81ff57ae10fa465b7b5f
MayaBenhous <165984997+MayaBenhous@users.noreply.github.com>
2024-06-07 10:14:27 +0300

    header

---
+++ exercise-3-javascript-MayaBenhous-master/js/scripts.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+}
+
+function initRectangles() {
+}
+
+function initSongs() {
+}
+
+function chooseRectangleColor() {
+}
+
+function addRectangle() {
+}
+
+function subtractRectangle() {
+}
+
+function switchRectanglesSongs() {
+    document.getElementById="type_page".onclick = function() {
+        let type = span.textContent;
+        console.log(type);
+    }
+}
+
+function populateSongsInList() {
+}
