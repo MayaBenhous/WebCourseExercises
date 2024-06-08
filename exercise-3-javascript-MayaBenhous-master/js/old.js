676e1585710855562f341d13b53e0a20ee0f96dc
MayaBenhous <165984997+MayaBenhous@users.noreply.github.com>
2024-06-08 19:02:53 +0300

    change of songs page, from file json

---
+++ exercise-3-javascript-MayaBenhous-master/js/old.js
@@ -0,0 +1,18 @@
+
+// if (document.getElementById("songs") == true) {
+//   document.getElementById("songs").style.visibility = "hidden";
+//   document.getElementById("rectangles").style.visibility = "visible";
+// } else {
+//   document.getElementById("songs").style.visibility = "visible";
+//   document.getElementById("rectangles").style.visibility = "hidden";
+// }
+// document.getElementById("rectangles").style.visibility = "hidden";
+// const rectangles = document.getElementById("rectangles_wrapper");
+// const colors = ["#F0F8FF", "#7FFFD4", "#00FFFF", "#6495ED", "#E9967A"];
+// const name = ["M", "a", "y", "a", "B", "e", "n", "h", "o", "u", "s"];
+// document.getElementById("songs").style.visibility = "visible";
+// document.getElementsByClassName("type_page").onclick = () => {
+//   console.log("CLICK");
+// };
+// "category2": "box_name",
+//   "Letters": ["M", "a", "y", "a", "B", "e", "n", "h", "o", "u", "s"]
--- exercise-3-javascript-MayaBenhous-master/js/scripts.js
+++ exercise-3-javascript-MayaBenhous-master/js/scripts.js
@@ -1,27 +1,45 @@
 window.onload = () => {
-}
+  initSongs();
+};
 
 function initRectangles() {
+  const name = ["M", "a", "y", "a", "B", "e", "n", "h", "o", "u", "s"];
 }
 
 function initSongs() {
+  fetch("data/music.json")
+    .then((response) => response.json())
+    .then((data) => populateSongsInList(data));
 }
 
 function chooseRectangleColor() {
+  const colors = ["#F0F8FF", "#7FFFD4", "#00FFFF", "#6495ED", "#E9967A"];
 }
 
 function addRectangle() {
+  const wrapper = document.getElementById("wrapper");
+  const songTitle = document.createElement("section");
+  
 }
 
-function subtractRectangle() {
-}
+function subtractRectangle() {}
 
-function switchRectanglesSongs() {
-    document.getElementById="type_page".onclick = function() {
-        let type = span.textContent;
-        console.log(type);
-    }
-}
+function switchRectanglesSongs() {}
 
-function populateSongsInList() {
+function populateSongsInList(data) {
+  const wrapper = document.getElementById("wrapper");
+  const songTitle = document.createElement("h1");
+  wrapper.appendChild(songTitle);
+  document.querySelector("h1").innerHTML = `${data.musicTitle}`;
+  const ulFrag = document.createDocumentFragment();
+  const songsList = document.createElement("ul");
+  ulFrag.appendChild(songsList);
+  for (const s in data.songs) {
+    const songItem = document.createElement("li");
+    const song = data.songs[s];
+    const songStr = `${song.id}. ${song.artist} - ${song.songName}`;
+    songItem.innerHTML = songStr;
+    songsList.appendChild(songItem);
+  }
+  wrapper.appendChild(ulFrag);
 }
