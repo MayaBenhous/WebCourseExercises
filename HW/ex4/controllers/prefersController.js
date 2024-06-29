c313551ba5c0cd5be494af3f383661f28dda390c
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-06-29 21:24:37 +0300

    add new prefer

--- HW/ex4/controllers/prefersController.js
+++ HW/ex4/controllers/prefersController.js
@@ -1,65 +1,101 @@
+const { dbConnection } = require("../db_connection");
+const vacationData = require("../data/records.json");
+
 exports.prefersController = {
-  // GET localhost:8081/posts
-  // async getPosts() {
-  //   const { dbConnection } = require("../db_connection");
+  async postNewPrefer(req, res) {
+    const connection = await dbConnection.createConnection();
+    const { body } = req;
+    try {
+      const access_code_user = body.access_code;
+      const access_code_tblUsers = await getUserById(req.params.idUser,connection);
+      let sameAccessCode = (access_code_user === access_code_tblUsers.access_code);
+
+      console.log(sameAccessCode);
+      if (!sameAccessCode) {
+        res.status(400).json({ error: "Connection failed! User not exist." });
+      }
+
+      let destExist = checkDestExist(body.destination, vacationData);
+      console.log(destExist);
+      if (!destExist) {
+        res.status(400).json({ error: "Connection failed! Destination not from list." });
+      }
+
+      let typeExist = checkTypeExist(body.type_vacation, vacationData);
+      console.log(typeExist);
+      if (!typeExist) {
+        res.status(400).json({ error: "Connection failed! Type not from list." });
+      }
+
+      if (sameAccessCode && destExist && typeExist) {
+        console.log("good!");
+        await connection.execute(
+          `INSERT INTO tbl_40_preferences (access_code, start_date, end_date, destination, type_vacation) VALUES 
+          ("${body.access_code}", "${body.start_date}", "${body.end_date}", "${body.destination}", "${body.type_vacation}")`
+        );
+          res
+            .status(201)
+            .json({ success: `connection success!` });
+        // }
+      } else {
+        res.status(400).json({ error: "Connection failed! User/ dest/ type not right." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", details: error.message });
+    }
+    connection.end();
+  },
+
+  // async putUpdatePrefer(req, res) {
   //   const connection = await dbConnection.createConnection();
-  //   const [rows] = await connection.execute(`SELECT * from tbl_40_preferences`);
+
   //   connection.end();
-  //   return rows;
   // },
 
-  async postNewPrefer(req, res) {
-    const { dbConnection } = require("../db_connection");
-    const connection = await dbConnection.createConnection();
-    let destOK = false;
-    let typeOk = false;
-    try {
-      const { accessCode, start_date, end_date, destination, type_vacation } = req.body;
-      while (!destOK && !typeOk) {
-        destOK = checkDestExist(destination, vacationData);
-        typeOk = checkTypeExist(type_vacation, vacationData);
-      }
-      await connection.execute(
-        `INSERT INTO tbl_40_preferences (access_code, start_date, end_date, destination, type_vacation) VALUES  ("${accessCode}", "${start_date}", "${end_date}", "${destination}", "${type_vacation}")`
-      );
-      res.status(201).json({ accessCode: accessCode });
-    } catch (error) {
-      res.status(500).json({ Error: "user already exist" });
-    }
+  // async getViewAllPrefers(req, res) {
+  //   const connection = await dbConnection.createConnection();
 
-    connection.end();
-  },
-
-  async putUpdatePrefer(req, res) {
-    const { dbConnection } = require("../db_connection");
-    const connection = await dbConnection.createConnection();
-
-    // const [rows] = await connection.execute(`SELECT * from tbl_40_preferences`);
-    connection.end();
-    // return rows;
-  },
-
-  async getViewAllPrefers(req, res) {
-    const { dbConnection } = require("../db_connection");
-    const connection = await dbConnection.createConnection();
-    // const [rows] = await connection.execute(`SELECT * from tbl_40_preferences`);
-    connection.end();
-    // return rows;
-  },
+  //   connection.end();
+  // },
 };
 
-function checkDestExist(destination, vacationData) {
-  for (let v in vacationData.destinations) {
+async function getUserById(userId, connection) {
+  const [rows] = await connection.execute(
+    `SELECT access_code from tbl_40_users WHERE id=${userId}`
+  );
+  return rows[0];
+}
+
+async function checkDestExist(destination, vacationData) {
+  const lengthDestinations = vacationData.destinations.length;
+  for (let v = 0; v < lengthDestinations; v++) {
     const vacation_dest = vacationData.destinations[v];
-    if (vacation_dest.dest_name == destination) return true;
+    if (vacation_dest.dest_name == destination) {
+      return true;
+    }
   }
   return false;
 }
 
-function checkTypeExist(type, vacationData) {
-  for (let t in vacationData.types) {
-    const vacation_type = vacationData.types[t];
-    if (vacation_type.type_name == destination) return true;
+async function checkTypeExist(type, vacationData) {
+  const lengthTypes = vacationData.types.length;
+  for (let v = 0; v < lengthTypes; v++) {
+    const vacation_type = vacationData.types[v];
+    if (vacation_type.type_name == type) return true;
   }
   return false;
 }
+
+// const { accessCode, start_date, end_date, destination, type_vacation } = req.body;
+// while (destOK == false && typeOk == false) {
+//   if (destOK == false) {
+//     // destOK = checkDestExist(destination, vacationData);
+//     res.status(500).json({ Error: "dest don't exist" });
+//   }
+//   if (typeOk == false) {
+//     // typeOk = checkTypeExist(type_vacation, vacationData);
+//     res.status(500).json({ Error: "type don't exist" });
+//   }
+// }
