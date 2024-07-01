d099ba2bd5c6a55198d5e3819fda44d6ea02b0f8
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-07-01 23:33:03 +0300

    index use funcs

--- HW/HW4/index.js
+++ HW/HW4/index.js
@@ -6,3 +6,23 @@ const port = process.env.PORT || 8080;
 const { usersRouter } = require("./routers/usersRouter.js");
 const { prefersRouter } = require("./routers/prefersRouter.js");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.set('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
+    res.set('Content-Type', 'application/json');
+    next();
+});
+
+app.use('/api/users', usersRouter);
+app.use('/api/preferences', prefersRouter);
+
+app.use((req, res) => {
+    res.status(400).send('Something is broken!');
+});
+
+app.listen(port);
+console.log(`listening on port ${port}`);
