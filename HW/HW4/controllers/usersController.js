64e9c6a7dac3cc49f35b0893716c5a47f80ac874
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-07-01 23:38:52 +0300

    users controller func NewUser

--- HW/HW4/controllers/usersController.js
+++ HW/HW4/controllers/usersController.js
@@ -2,6 +2,19 @@ const { dbConnection } = require("../db_connection");
 const { v4: uuidv4 } = require('uuid');
 
 exports.usersController = {
+  async postNewUser(req, res) {
+    const connection = await dbConnection.createConnection();
+    try {
+        const {user_name, password} = req.body;
+        const accessCode = uuidv4();
+        await connection.execute(`INSERT INTO tbl_40_users (access_code, user_name, password) VALUES  ("${accessCode}", "${user_name}", "${password}")`);
+        res.status(201).json({ accessCode: accessCode });
+    }
+    catch (error) {
+        res.status(500).json({ Error: 'user already exist' });
+      }
+    connection.end();
+  },
 
 };
 
