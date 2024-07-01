0cf78d355df5d50b3e1fa2dca3de3f62743359c3
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-07-01 23:35:06 +0300

    router of users DB

--- HW/HW4/routers/usersRouter.js
+++ HW/HW4/routers/usersRouter.js
@@ -1,8 +1,3 @@
 const { Router } = require("express");
 const { usersController } = require("../controllers/usersController.js");
-
 const usersRouter = new Router();
-
-usersRouter.post("/register", usersController.postNewUser);
-
-module.exports = { usersRouter };
