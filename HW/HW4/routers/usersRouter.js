5af80f3e1120e9cb259006cd5628105eee693e2e
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-07-01 23:35:34 +0300

    final funcs of users router

--- HW/HW4/routers/usersRouter.js
+++ HW/HW4/routers/usersRouter.js
@@ -1,3 +1,7 @@
 const { Router } = require("express");
 const { usersController } = require("../controllers/usersController.js");
 const usersRouter = new Router();
+
+usersRouter.post("/register", usersController.postNewUser);
+
+module.exports = { usersRouter };
