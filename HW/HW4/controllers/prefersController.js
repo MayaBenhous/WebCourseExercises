0be5079057a9d095c6abad5b9868ab7385d4bef3
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-07-01 23:43:40 +0300

    putUpdatePrefer.js func without funcs out

--- HW/HW4/controllers/prefersController.js
+++ HW/HW4/controllers/prefersController.js
@@ -52,6 +52,81 @@ exports.prefersController = {
     }
     connection.end();
   },
+
+  async putUpdatePrefer(req, res) {
+    const connection = await dbConnection.createConnection();
+    const { body } = req;
+    try {
+      const access_code_user = body.access_code;
+      const access_code_tblUsers = await getUserById(
+        req.params.idUser,
+        connection
+      );
+      let sameAccessCode =
+        access_code_user === access_code_tblUsers.access_code;
+      if (!sameAccessCode) {
+        res
+          .status(400)
+          .json({
+            error: "Connection failed! access_code is not match to this user.",
+          });
+      }
+      let update_changes = [
+        body.access_code,
+        body.start_date,
+        body.end_date,
+        body.destination,
+        body.type_vacation,
+      ];
+      const vocation_fields = [
+        "access_code",
+        "start_date",
+        "end_date",
+        "destination",
+        "type_vacation",
+      ];
+      if (sameAccessCode) {
+        for (let i = 0; i < update_changes.length; i++) {
+          if (!update_changes[i])
+            if (i === 1) {
+              update_changes[1] = await getStartDate(update_changes[0],connection);
+            } else if (i === 2) {
+              update_changes[2] = await getEndDate(update_changes[0],connection);
+            } else {
+              update_changes = vocation_fields[i];
+            }
+          console.log(update_changes[i]);
+        }
+        let validDates = true;
+        // let validDates = await checkDatesValid(update_changes[1], update_changes[2]);
+        if (validDates === false) {
+          res.status(400).json({ error: "Update failed! Dates not valid." });
+        }
+        let destExist = await checkDestExist(update_changes[3], vacationData);
+        if (!destExist) {
+          res.status(400).json({ error: "Update failed! Destination not from list." });
+        }
+        let typeExist = await checkTypeExist(update_changes[4], vacationData);
+        if (!typeExist) {
+          res.status(400).json({ error: "Update failed! Type not from list." });
+        }
+        await connection.execute(
+          `UPDATE tbl_40_preferences SET start_date="${update_changes[1]}", end_date="${update_changes[2]}", destination="${update_changes[3]}",
+            type_vacation="${update_changes[4]}", curr_time=now() WHERE access_code="${update_changes[0]}"`
+        );
+        console.log("good job!");
+        res
+          .status(201)
+          .json({ success: `Update preference is in! - connection success!` });
+      } else {
+        res.status(400).json({ error: "Connection failed! User not exist." });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+    connection.end();
+  },
+
 }
 
 async function checkDestExist(destination, vacationData) {
